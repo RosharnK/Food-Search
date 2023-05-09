06e947acaf253b41cc96a53a2ddfffad5b3fd1af
INFINITYPROGRAMMERYT <infinityprogrammerofficial@gmail.com>
2023-05-10 01:13:37 +0530

    bugs remove

--- utils/firebase.js
+++ utils/firebase.js
@@ -12,13 +12,6 @@ const firebaseConfig = {
   messagingSenderId: "600931697760",
   appId: "1:600931697760:web:3105ec528a509d7ea46f05",
   measurementId: "G-9B3R37Y1FH",
-  apiKey: "AIzaSyAJXnoYks0R3u86u1IbKmUKBg7Gje4Zx28",
-  authDomain: "food-search-d9ee7.firebaseapp.com",
-  projectId: "food-search-d9ee7",
-  storageBucket: "food-search-d9ee7.appspot.com",
-  messagingSenderId: "600931697760",
-  appId: "1:600931697760:web:3105ec528a509d7ea46f05",
-  measurementId: "G-9B3R37Y1FH",
 };
 
 // Initialize Firebase
